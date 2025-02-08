c4b40e3e3d2361f9967c07ce66d0c5e40f0f1121
suraj patel <sp9589890205@gmail.com>
2025-02-08 23:49:45 +0530

    [Suraj] added Recursive vs Iterative Fibonacci Computation

---
+++ src/main/java/com/algoruntimeanalysis/fibonaccicomputation/RecursiveFibonacciSeries.java
@@ -0,0 +1,10 @@
+package com.algoruntimeanalysis.fibonaccicomputation;
+
+public class RecursiveFibonacciSeries {
+    //recursive method  to find nth fibonacci number
+    public static int fibonacciRecursive(int n) {
+        //base case
+        if (n <= 1) return n;
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+}
