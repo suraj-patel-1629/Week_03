da4137b6ffa70d66ece12cac9d44f08f3db0dd34
suraj patel <sp9589890205@gmail.com>
2025-02-08 23:52:38 +0530

    [Suraj] added Comparing Different Data Structures for Searching

---
+++ src/main/java/com/algoruntimeanalysis/searchcomparison/TreeSetComparison.java
@@ -0,0 +1,10 @@
+package com.algoruntimeanalysis.searchcomparison;
+
+import java.util.TreeSet;
+
+public class TreeSetComparison {
+    //method to search element at TreeSet
+    public static boolean search(TreeSet<Integer> treeSet, int key) {
+        return treeSet.contains(key);
+    }
+}
